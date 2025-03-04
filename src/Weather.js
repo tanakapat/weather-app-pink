fcc48e9fb35db84d0cf12bf9315c6048df70373c
tanakapat <patricia.tanaka.3@hotmail.com>
2025-03-04 13:37:07 -0300

    Added React App and testing the components

---
+++ src/Weather.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Weather() {
+  return (
+   <div className="weatherApp">
+    <h1 className="hey">HELLO</h1>
+   </div>
+  );
+}
+
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './Weather.css';
+import Weather from './Weather';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    
+      <Weather />
+    
   </React.StrictMode>
 );
 
